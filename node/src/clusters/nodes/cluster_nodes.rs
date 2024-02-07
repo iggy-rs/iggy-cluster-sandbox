9de78fd7732efca637892d4a33eea95a1b11f7e8
spetz <piotr.gankiewicz@gmail.com>
2024-02-07 05:52:15 +0100

    Validate if node can be elected as leader

--- node/src/clusters/nodes/cluster_nodes.rs
+++ node/src/clusters/nodes/cluster_nodes.rs
@@ -1,7 +1,8 @@
 use crate::clusters::cluster::Cluster;
+use crate::clusters::nodes::node::Node;
 use sdk::error::SystemError;
 use std::collections::HashMap;
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 impl Cluster {
     pub async fn sync_nodes_state(&self) -> Result<(), SystemError> {
@@ -33,10 +34,70 @@ impl Cluster {
         }
         info!("Synced state from cluster nodes.");
 
-        // TODO: Validate other nodes states and check if this node can be a leader.
-        let self_node = self_node.unwrap();
-        self_node.node.set_can_be_leader(true).await;
+        let mut nodes_by_last_applied = HashMap::new();
+        for (node_id, state) in states.iter() {
+            let last_applied = state.last_applied;
+            let nodes = nodes_by_last_applied
+                .entry(last_applied)
+                .or_insert(Vec::new());
+            nodes.push(node_id);
+        }
 
+        let self_node = self_node.unwrap();
+        if nodes_by_last_applied.is_empty() {
+            info!("This node cannot be a leader because there are no other nodes in the cluster.");
+            set_leader_unavailable(&self_node.node).await;
+            return Ok(());
+        }
+
+        if states.iter().all(|(_, state)| state.last_applied == 0) {
+            info!("This node can be a leader because it's state is initial state of the cluster.");
+            set_leader_available(&self_node.node).await;
+            return Ok(());
+        }
+
+        let min_last_applied = *nodes_by_last_applied.keys().min().unwrap();
+        let self_state = self.get_node_state().await.unwrap();
+        if self_state.last_applied < min_last_applied {
+            set_leader_unavailable(&self_node.node).await;
+            warn!("This node cannot be a leader because it's state is behind other nodes, last applied: {} < {}", self_state.last_applied, min_last_applied);
+            return Ok(());
+        }
+
+        let nodes_with_lower_last_applied = nodes_by_last_applied
+            .iter()
+            .filter(|(last_applied, _)| *last_applied < &self_state.last_applied)
+            .map(|(_, nodes)| nodes.len() as u64)
+            .sum::<u64>();
+
+        let nodes_with_same_last_applied = 1 + nodes_by_last_applied
+            .get(&self_state.last_applied)
+            .unwrap_or(&Vec::new())
+            .len() as u64;
+
+        let quorum = self.get_quorum_count();
+        if nodes_with_lower_last_applied + nodes_with_same_last_applied < quorum {
+            set_leader_unavailable(&self_node.node).await;
+            warn!("This node cannot be a leader because it's state is behind other nodes, last applied: {} < {}", self_state.last_applied, min_last_applied);
+            return Ok(());
+        }
+
+        let all_last_applied_states = states
+            .iter()
+            .map(|(node_id, state)| format!("{} -> {}", node_id, state.last_applied))
+            .collect::<Vec<String>>()
+            .join(", ");
+        set_leader_available(&self_node.node).await;
+        info!("This node can be a leader because it's state: {} is up to date with other nodes, all last applied states by nodes: {all_last_applied_states}", self_state.last_applied);
         Ok(())
     }
+}
+
+async fn set_leader_unavailable(node: &Node) {
+    node.set_can_be_leader(false).await;
+    // TODO: Handle data sync
+}
+
+async fn set_leader_available(node: &Node) {
+    node.set_can_be_leader(true).await;
 }
